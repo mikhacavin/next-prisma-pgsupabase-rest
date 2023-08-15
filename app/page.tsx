ac8eb3c016651b71e73a4ad51e8bd3dc13c8802d
mikhacavin <mikhacavinarthur@gmail.com>
2023-08-15 14:05:17 +0700

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
    <section>
-    Nextjs Prisma Rest API With PostgreSQL Neon. <br />
+    Nextjs Prisma Rest API With Neon PostgreSQL. <br />
     By <Link href={"https://mikhacavin.com"}>Mikha Cavin</Link> <br />
     visit : <Link href={'/api/blogs'} className="button" rel="noopener noreferrer" target="_blank">API</Link>
    </section>
